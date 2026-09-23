2bf0b60b1329dcdef516f71c97b12db19dfd31b3
robe-izanagi <robertofarol10145@gmail.com>
2026-09-23 11:13:09 +0800

    feat(admin): add protected feedback route and nested layout routing

--- web-admin/src/App.jsx
+++ web-admin/src/App.jsx
@@ -7,32 +7,37 @@ import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import AccountCodes from "./pages/AccountCodes";
 import Declaration from "./pages/Declaration";
-import "./App.css";
 import ManageUsers from "./pages/ManageUser";
+import Feedback from "./pages/Feedback";
+import "./App.css";
 
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
+          {/* Public routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+
+          {/* Protected routes */}
           <Route
-            path="/*"
             element={
               <RequireAuth>
-                <Layout>
-                  <Routes>
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/account-codes" element={<AccountCodes />} />
-                    <Route path="/declaration" element={<Declaration />} />
-                    <Route path="/users" element={<ManageUsers />} />
-                    <Route path="*" element={<Navigate to="/login" />} />
-                  </Routes>
-                </Layout>
+                <Layout />
               </RequireAuth>
             }
-          />
+          >
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/account-codes" element={<AccountCodes />} />
+            <Route path="/declaration" element={<Declaration />} />
+            <Route path="/users" element={<ManageUsers />} />
+            <Route path="/feedback" element={<Feedback />} />
+          </Route>
+
+          {/* Default */}
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
